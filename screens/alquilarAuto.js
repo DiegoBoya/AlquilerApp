a301108ce2504300f905bdc292855246e84b70e5
diego boya <dboyatjian@gmail.com>
2021-07-09 19:51:44 -0300

    screen de auto

---
+++ screens/alquilarAuto.js
@@ -0,0 +1,4 @@
+import React , {useContext, useState, useEffect} from 'react';
+import { StyleSheet, Text, View, FlatList, Button, Alert, TouchableOpacity, Image, TextInput } from 'react-native';
+import {AuthContext} from './Context';
+
